2f9d1f04ed4ac0add82011b8b02129aa82a696b0
nguyen xuan tien <nguyen.xuan.tien096@gmail.com>
2024-05-03 16:18:12 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so tien Do-la My");
+        float Dola = Convert.ToSingle(Console.ReadLine());
+        float VND = Dola * 23000;
+        if (Dola >= 0)
+        {
+            Console.WriteLine("So tien chuyen doi la" + VND);
+        } else 
+        {
+            Console.WriteLine("Nhap vao so tien chinh xac");
+        }    
+    }
+}
